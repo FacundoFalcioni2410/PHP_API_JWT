a138ca7710eb9f68a638d73d45244c602b24a942
FacundoFalcioni2410 <facundofalcioni2410@gmail.com>
2021-07-11 18:13:35 -0300

    api terminada 100%

--- public/index.php
+++ public/index.php
@@ -21,17 +21,5 @@
     $app->delete('/', \Auto::class . ":EliminarAuto")->add(\MW::class . ":VerificarPropietario")->add(\MW::class . ":VerificarToken");
     $app->put('/', \Auto::class . ":ModificarAuto")->add(\MW::class . ":VerificarEncargadoYPropietario")->add(\MW::class . ":VerificarToken");
     
-    // $app->post('/login[/]', \Verificadora::class . ':VerificarUsuario')->add(\Verificadora::class . ':ValidarParametrosUsuario');
-    // $app->get('/login/test', \Verificadora::class . ':ObtenerDataJWT')->add(\Verificadora::class . ':ChequearJWT');
-
-    // $app->group('/json_bd', function(RouteCollectorProxy $grupo)
-    // {
-    //     $grupo->get('/', \cd::class . ':TraerTodos');
-    //     $grupo->get('/{id}', \cd::class . ':TraerUno');
-    //     $grupo->post('/', \cd::class . ':Agregar')->add(\Verificadora::class . ':ValidarParametrosCDAgregar');
-    //     $grupo->put('/', \cd::class . ':Modificar')->add(\Verificadora::class . ':ValidarParametrosCDModificar');
-    //     $grupo->delete('/', \cd::class . ':Eliminar')->add(\Verificadora::class . ':ValidarParametrosCDEliminar');
-    // })->add(\Verificadora::class . ':ChequearJWT');
-
     $app->run();
 ?>
